432fb75e6d66434b38293051d09e644e66568b86
Samftc <sbigham2022@crossroadscollegeprep.org>
2021-01-11 12:16:26 -0600

    TestOmni

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOmni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOmni.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "ArmOmni")
+@TeleOp(name = "TestOmni")
 public class TestOmni extends OpMode {
     DcMotor A;
     DcMotor FL;
