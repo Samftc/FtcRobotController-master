eb842e984d80ca0b8426ecb8077f0f7047a9d80b
Samftc <sbigham2022@crossroadscollegeprep.org>
2021-02-06 11:55:37 -0600

    Debug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Debug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Debug.java
@@ -27,7 +27,7 @@ public class Debug extends LinearOpMode {
     static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double DRIVE_SPEED = 0.1;
+    static final double DRIVE_SPEED = 0.3;
     static final double TURN_SPEED = 0.25;
 
 
@@ -89,7 +89,7 @@ public class Debug extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED, 48, 48, 3.6);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(DRIVE_SPEED, 48, 48, 1.21);  // S1: Forward 47 Inches with 5 Sec timeout
       //  encoderDrive(TURN_SPEED, 12, -12, .5);  // S2: Turn Right 12 Inches with 4 Sec timeout
        // encoderDrive(DRIVE_SPEED, -24, -24, .5);  // S3: Reverse 24 Inches with 4 Sec timeout
 
