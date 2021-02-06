b7a93f42c54e130863cecebb8e91f43615983cb2
eryan40 <68960832+eryan40@users.noreply.github.com>
2021-02-06 10:59:33 -0600

    counts per motor rev = 1120, wheel diameter = 12

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyAuto.java
@@ -22,9 +22,9 @@ public class EasyAuto extends LinearOpMode {
 
 
     private ElapsedTime runtime = new ElapsedTime();
-    static final double COUNTS_PER_MOTOR_REV = 373;    // eg: TETRIX Motor Encoder
+    static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: TETRIX Motor Encoder
     static final double DRIVE_GEAR_REDUCTION = .1;     // This is < 1.0 if geared UP
-    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double WHEEL_DIAMETER_INCHES = 12.0;     // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double DRIVE_SPEED = 0.6;
