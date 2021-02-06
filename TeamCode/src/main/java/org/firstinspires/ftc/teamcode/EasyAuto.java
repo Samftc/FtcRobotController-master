5daa82638da62ee7695a1934c78d128f408596ff
eryan40 <68960832+eryan40@users.noreply.github.com>
2021-02-06 10:11:20 -0600

    changed counts per motor revolution

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyAuto.java
@@ -22,7 +22,7 @@ public class EasyAuto extends LinearOpMode {
 
 
     private ElapsedTime runtime = new ElapsedTime();
-    static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    static final double COUNTS_PER_MOTOR_REV = 20;    // eg: TETRIX Motor Encoder
     static final double DRIVE_GEAR_REDUCTION = 2.0;     // This is < 1.0 if geared UP
     static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
